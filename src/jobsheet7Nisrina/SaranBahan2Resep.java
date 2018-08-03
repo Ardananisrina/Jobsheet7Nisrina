75b872b393e2ae27f85173d5ccc7e3d7d32ea7de
Nisrina Izdihar <Nisrina Izdihar@DESKTOP-UDM8N91>
2018-08-03 12:56:19 +0700

    Initial Commit

---
+++ src/jobsheet7Nisrina/SaranBahan2Resep.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet7Nisrina;
+
+/**
+ *
+ * @author Nisrina Izdihar
+ */
+public class SaranBahan2Resep {
+    public static void main(String[] args) {
+    String identitas = "Nisrina Izdihar / XRPL5 / 32";
+    System.out.println("identitas : " + identitas);
+    
+    System.out.print("\nSaran Resep dari Bahan Milik Anda\n");
+    
+    }
+}
