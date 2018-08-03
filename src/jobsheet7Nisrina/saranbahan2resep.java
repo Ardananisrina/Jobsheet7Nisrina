88587b47ac3baecf69f84900511a4038c0b53130
Nisrina Izdihar <Nisrina Izdihar@DESKTOP-UDM8N91>
2018-08-03 20:10:19 +0700

    Tambah Tampil Resep

--- src/jobsheet7Nisrina/saranbahan2resep.java
+++ src/jobsheet7Nisrina/saranbahan2resep.java
@@ -21,6 +21,45 @@ public class saranbahan2resep {
         System.out.println("2. telur");
         System.out.print("masukkan no pilihan anda : ");
         int bahan1 = scanner.nextInt();
+        
+        if(bahan1 == 1 || bahan1 == 2)
+        {
+            System.out.println("bahan kedua : ");
+            if(bahan1 == 1) {
+                System.out.println("1. susu");
+                System.out.println("2. minyak goreng");
+                System.out.println("3. tidak ada");
+            }else{
+                System.out.println("1. minyak goreng");
+                System.out.println("2. roti");
+                System.out.println("3. tidak ada");
+            }
+            System.out.print("masukkan no pilihan anda :");
+            
+        }else
+            System.out.println("mohon maaf, pilihan tidak ditemukan," + "tidak dapat memberikan saran resep");
+        
+        int bahan2 = scanner.nextInt();
+        if(bahan2 >= 1 && bahan2 <= 3);
+        {
+            if(bahan1==1)
+                switch(bahan1)
+                {
+                    case 1 : System.out.println("anda dapat membuat milk shake banana"); break;
+                    case 2 : System.out.println("anda dapat membuat pisang goreng"); break;
+                    case 3 : System.out.println("anda bisa membuat pisang rebus"); break;
+                }
+            else
+                switch(bahan1)
+                {
+                    case 1 : System.out.println("anda dapat membuat telur mata sapi"); break;
+                    case 2 : System.out.println("anda dapat membuat sandwich telur"); break;
+                    case 3 : System.out.println("anda dapat mebuat telur rebus"); break;
+                    
+                }
+        }else
+          System.out.println("mohon maaf, pilihan tidak ditemukan," + "tidak dapat memberikan saran resep");
+        }
     }
     
-}
+
