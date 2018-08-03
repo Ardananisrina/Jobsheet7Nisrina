91da5c64b392c4f20ea0ceb92131d5806b61103d
Nisrina Izdihar <Nisrina Izdihar@DESKTOP-UDM8N91>
2018-08-03 12:37:13 +0700

    coba SWITCH..CASE.

--- src/jobsheet7Nisrina/cobapercabangan.java
+++ src/jobsheet7Nisrina/cobapercabangan.java
@@ -67,6 +67,15 @@ public class cobapercabangan {
                 }
             }
         }
+        System.out.println("\nSWITCH..CASE");
+        switch(a) {
+            case 1 : System.out.println("nilai a => 1"); break;
+            case 2 : System.out.println("nilai a => 2"); break;
+            case 3 : 
+            case 4 : System.out.println("nilai a => 3 atau 4"); break;
+            case 5 : System.out.println("nilai a => 5"); break;
+            default : System.out.println("nilai a bukan antara");break;
     }
     
+}
 }
