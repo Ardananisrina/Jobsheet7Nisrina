5393e5178d704668c9d62f42227151727190d64a
Nisrina Izdihar <Nisrina Izdihar@DESKTOP-UDM8N91>
2018-08-03 18:11:44 +0700

    Initial commit

---
+++ src/jobsheet7Nisrina/cobapercabangan2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet7Nisrina;
+
+/**
+ *
+ * @author Nisrina Izdihar
+ */
+public class cobapercabangan2 {
+    public static void main(String[] args) {
+        String identitas = "nama / kelas / no";
+        System.out.println("identitas : " + identitas);
+        
+        
+    }
+    
+}
