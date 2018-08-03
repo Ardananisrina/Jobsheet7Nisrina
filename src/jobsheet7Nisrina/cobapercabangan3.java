e0777e2b1b7732eae5c5ec70b652b35542835628
Nisrina Izdihar <Nisrina Izdihar@DESKTOP-UDM8N91>
2018-08-03 20:52:00 +0700

    Tambah Input Bahan 1

--- src/jobsheet7Nisrina/cobapercabangan3.java
+++ src/jobsheet7Nisrina/cobapercabangan3.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package jobsheet7Nisrina;
-
+import java.util.Scanner;
 /**
  *
  * @author Nisrina Izdihar
@@ -14,7 +14,13 @@ public class cobapercabangan3 {
         String identitas = "Nisrina Izdihar / XRPL5 / 32";
         System.out.println("identitas : " + identitas);
         
-       
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("bahan pertama : ");
+        System.out.println("1. pisang");
+        System.out.println("2. telur");
+        System.out.print("masukkan no pilihan anda : ");
+        int bahan1 = scanner.nextInt();
         
         
                 
